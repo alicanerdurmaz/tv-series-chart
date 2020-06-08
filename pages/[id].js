36cfec1b15591aa9041aaedd20f9ecfac9b28144
alicanerdurmaz <alicanerdurmaz@gmail.com>
2020-06-08 14:20:51 +0300

    env

--- pages/[id].js
+++ pages/[id].js
@@ -18,3 +18,53 @@ export default function TvSeries() {
     </>
   );
 }
+
+export async function getServerSideProps(context) {
+  const searchParam = context.params.id;
+
+  const url = [`http://omdbapi.com/?apikey=8ab88a20&t=${NEXT_PUBLIC_OMDB_API_KEY}`];
+
+  const res = await fetch(url[0]);
+  const tvSeries = await res.json();
+  const totalSeasons = tvSeries.totalSeasons;
+
+  const rawSeasonsData = [];
+
+  const seasonUrl = [];
+  for (let i = 1; i <= totalSeasons; i++) {
+    seasonUrl.push(`http://omdbapi.com/?apikey=${NEXT_PUBLIC_OMDB_API_KEY}&t=${searchParam}&season=${i}`);
+  }
+
+  await Promise.all(
+    seasonUrl.map(async (seasonUrl) => {
+      const response = await fetch(seasonUrl);
+      const result = await response.json();
+      rawSeasonsData.push(result);
+    })
+  );
+
+  function compare(a, b) {
+    if (parseInt(a.Season) < parseInt(b.Season)) {
+      return -1;
+    }
+    if (parseInt(a.Season) > parseInt(b.Season)) {
+      return 1;
+    }
+    return 0;
+  }
+  rawSeasonsData.sort(compare);
+
+  const seasonData = [];
+
+  let counter = 1;
+  rawSeasonsData.forEach((s) => {
+    s.Episodes.forEach((e) => {
+      e.episodeWithSeason = 'S' + s.Season + 'E' + e.Episode;
+      e.episodeNumber = counter;
+      counter++;
+      seasonData.push(e);
+    });
+  });
+
+  return { props: { seasonData } };
+}
